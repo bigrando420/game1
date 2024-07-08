ea909f5ebda14593b2650439c1ddc103e4a320c3
randy <tom@randy.gg>
2024-07-08 18:33:01 +0700

    init

--- build.c
+++ build.c
@@ -26,7 +26,7 @@ typedef struct Context_Extra {
 //
 
 // this is a minimal starting point for new projects. Copy & rename to get started
-#include "oogabooga/examples/minimal_game_loop.c"
+// #include "oogabooga/examples/minimal_game_loop.c"
 
 // #include "oogabooga/examples/text_rendering.c"
 
@@ -34,7 +34,7 @@ typedef struct Context_Extra {
 // #include "oogabooga/examples/renderer_stress_test.c"
 
 // Randy's example game that he's building out as a tutorial for using the engine
-// #include "entry_randygame.c"
+#include "entry_randygame.c"
 
 // This is where you swap in your own project!
 // #include "entry_yourepicgamename.c"

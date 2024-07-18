e56212012d139f65e143b76de733443822c40062
randy <tom@randy.gg>
2024-07-18 12:26:45 +0700

    caveman UI progress

--- entry_randygame.c
+++ entry_randygame.c
@@ -6,7 +6,35 @@ inline float v2_dist(Vector2 a, Vector2 b) {
     return v2_length(v2_sub(a, b));
 }
 
+#define m4_identity m4_make_scale(v3(1, 1, 1))
+
+Vector2 range2f_get_center(Range2f r) {
+	return (Vector2) { (r.max.x - r.min.x) * 0.5 + r.min.x, (r.max.y - r.min.y) * 0.5 + r.min.y };
+}
+
 // ^^^ engine changes
+
+// the scuff zone
+
+Draw_Quad ndc_quad_to_screen_quad(Draw_Quad ndc_quad) {
+
+	// NOTE: we're assuming these are the screen space matricies.
+	Matrix4 proj = draw_frame.projection;
+	Matrix4 view = draw_frame.view;
+
+	Matrix4 ndc_to_screen_space = m4_identity;
+	ndc_to_screen_space = m4_mul(ndc_to_screen_space, m4_inverse(proj));
+	ndc_to_screen_space = m4_mul(ndc_to_screen_space, view);
+
+	ndc_quad.bottom_left = m4_transform(ndc_to_screen_space, v4(v2_expand(ndc_quad.bottom_left), 0, 1)).xy;
+	ndc_quad.bottom_right = m4_transform(ndc_to_screen_space, v4(v2_expand(ndc_quad.bottom_right), 0, 1)).xy;
+	ndc_quad.top_left = m4_transform(ndc_to_screen_space, v4(v2_expand(ndc_quad.top_left), 0, 1)).xy;
+	ndc_quad.top_right = m4_transform(ndc_to_screen_space, v4(v2_expand(ndc_quad.top_right), 0, 1)).xy;
+
+	return ndc_quad;
+}
+
+//
 
 // 0 -> 1
 float sin_breathe(float time, float rate) {
@@ -32,8 +60,14 @@ void animate_v2_to_target(Vector2* value, Vector2 target, float delta_t, float r
 	animate_f32_to_target(&(value->y), target.y, delta_t, rate);
 }
 
+Range2f quad_to_range(Draw_Quad quad) {
+	return (Range2f){quad.bottom_left, quad.top_right};
+}
+
 // ^^^ generic utils
 
+// :tweaks
+Vector4 bg_box_col = {0, 0, 0, 0.5};
 const int tile_width = 8;
 const float entity_selection_radius = 16.0f;
 const float player_pickup_radius = 20.0f;
@@ -95,6 +129,14 @@ SpriteID get_sprite_id_from_archetype(EntityArchetype arch) {
 		case arch_item_pine_wood: return SPRITE_item_pine_wood; break;
 		case arch_item_rock: return SPRITE_item_rock; break;
 		default: return 0;
+	}
+}
+
+string get_archetype_pretty_name(EntityArchetype arch) {
+	switch (arch) {
+		case arch_item_pine_wood: return STR("Pine Wood");
+		case arch_item_rock: return STR("Rock");
+		default: return STR("nil");
 	}
 }
 
@@ -174,6 +216,21 @@ void setup_item_rock(Entity* en) {
 	en->arch = arch_item_rock;
 	en->sprite_id = SPRITE_item_rock;
 	en->is_item = true;
+}
+
+Vector2 get_mouse_pos_in_ndc() {
+	float mouse_x = input_frame.mouse_x;
+	float mouse_y = input_frame.mouse_y;
+	Matrix4 proj = draw_frame.projection;
+	Matrix4 view = draw_frame.view;
+	float window_w = window.width;
+	float window_h = window.height;
+
+	// Normalize the mouse coordinates
+	float ndc_x = (mouse_x / (window_w * 0.5f)) - 1.0f;
+	float ndc_y = (mouse_y / (window_h * 0.5f)) - 1.0f;
+
+	return (Vector2){ ndc_x, ndc_y };
 }
 
 Vector2 screen_to_world() {
@@ -426,11 +483,19 @@ int entry(int argc, char **argv) {
 			}
 
 			const float icon_thing = 8.0;
-			const float padding = 2.0;
-			float icon_width = icon_thing + padding;
+			float icon_width = icon_thing;
 
-			float entire_thing_width_idk = item_count * icon_width;
-			float x_start_pos = (width/2.0)-(entire_thing_width_idk/2.0) + (icon_width * 0.5);
+			const int icon_row_count = 8;
+
+			float entire_thing_width_idk = icon_row_count * icon_width;
+			float x_start_pos = (width/2.0)-(entire_thing_width_idk/2.0);
+
+			// bg box rendering thing
+			{
+				Matrix4 xform = m4_identity;
+				xform = m4_translate(xform, v3(x_start_pos, y_pos, 0.0));
+				draw_rect_xform(xform, v2(entire_thing_width_idk, icon_width), bg_box_col);
+			}
 
 			int slot_index = 0;
 			for (int i = 0; i < ARCH_MAX; i++) {
@@ -441,12 +506,87 @@ int entry(int argc, char **argv) {
 
 					Matrix4 xform = m4_scalar(1.0);
 					xform = m4_translate(xform, v3(x_start_pos + slot_index_offset, y_pos, 0.0));
-					xform = m4_translate(xform, v3(-4, -4, 0.0));
-					draw_rect_xform(xform, v2(8, 8), COLOR_BLACK);
 
 					Sprite* sprite = get_sprite(get_sprite_id_from_archetype(i));
 
+					float is_selected_alpha = 0.0;
+
+					Draw_Quad* quad = draw_rect_xform(xform, v2(8, 8), v4(1, 1, 1, 0.2));
+					Range2f icon_box = quad_to_range(*quad);
+					if (range2f_contains(icon_box, get_mouse_pos_in_ndc())) {
+						is_selected_alpha = 1.0;
+					}
+
+					Matrix4 box_bottom_right_xform = xform;
+
+					xform = m4_translate(xform, v3(icon_width * 0.5, icon_width * 0.5, 0.0));
+
+					if (is_selected_alpha == 1.0)
+					{
+						float scale_adjust = 0.3; //0.1 * sin_breathe(os_get_current_time_in_seconds(), 20.0);
+						xform = m4_scale(xform, v3(1 + scale_adjust, 1 + scale_adjust, 1));
+					}
+					{
+						// could also rotate ...
+						// float adjust = PI32 * 2.0 * sin_breathe(os_get_current_time_in_seconds(), 1.0);
+						// xform = m4_rotate_z(xform, adjust);
+					}
+					xform = m4_translate(xform, v3(get_sprite_size(sprite).x * -0.5, get_sprite_size(sprite).y * -0.5, 0));
 					draw_image_xform(sprite->image, xform, get_sprite_size(sprite), COLOR_WHITE);
+
+					// draw_text_xform(font, STR("5"), font_height, box_bottom_right_xform, v2(0.1, 0.1), COLOR_WHITE);
+
+					// tooltip
+					if (is_selected_alpha == 1.0)
+					{
+						Draw_Quad screen_quad = ndc_quad_to_screen_quad(*quad);
+						Range2f screen_range = quad_to_range(screen_quad);
+						Vector2 icon_center = range2f_get_center(screen_range);
+
+						// icon_center
+						Matrix4 xform = m4_scalar(1.0);
+
+						// TODO - we're guessing at the Y box size here.
+						// in order to automate this, we would need to move it down below after we do all the element advance things for the text.
+						// ... but then the box would be drawing in front of everyone. So we'd have to do Z sorting.
+						// Solution for now is to just guess at the size and OOGA BOOGA.
+						Vector2 box_size = v2(40, 14);
+
+						// xform = m4_pivot_box(xform, box_size, PIVOT_top_center);
+						xform = m4_translate(xform, v3(box_size.x * -0.5, -box_size.y - icon_width * 0.5, 0));
+						xform = m4_translate(xform, v3(icon_center.x, icon_center.y, 0));
+						draw_rect_xform(xform, box_size, bg_box_col);
+
+						float current_y_pos = icon_center.y;
+						{
+							string title = get_archetype_pretty_name(i);
+							Gfx_Text_Metrics metrics = measure_text(font, title, font_height, v2(0.1, 0.1));
+							Vector2 draw_pos = icon_center;
+							draw_pos = v2_sub(draw_pos, metrics.visual_pos_min);
+							draw_pos = v2_add(draw_pos, v2_mul(metrics.visual_size, v2(-0.5, -1.0))); // top center
+
+							draw_pos = v2_add(draw_pos, v2(0, icon_width * -0.5));
+							draw_pos = v2_add(draw_pos, v2(0, -2.0)); // padding
+
+							draw_text(font, title, font_height, draw_pos, v2(0.1, 0.1), COLOR_WHITE);
+
+							current_y_pos = draw_pos.y;
+						}
+
+						{
+							string text = STR("x%i");
+							text = sprint(temp, text, item->amount);
+
+							Gfx_Text_Metrics metrics = measure_text(font, text, font_height, v2(0.1, 0.1));
+							Vector2 draw_pos = v2(icon_center.x, current_y_pos);
+							draw_pos = v2_sub(draw_pos, metrics.visual_pos_min);
+							draw_pos = v2_add(draw_pos, v2_mul(metrics.visual_size, v2(-0.5, -1.0))); // top center
+
+							draw_pos = v2_add(draw_pos, v2(0, -2.0)); // padding
+
+							draw_text(font, text, font_height, draw_pos, v2(0.1, 0.1), COLOR_WHITE);
+						}
+					}
 
 					slot_index += 1;
 				}
